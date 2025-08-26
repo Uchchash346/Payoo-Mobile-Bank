54819f36882a51fd1c4aa0fd6843d9c45dc6085c
Uchchash346 <mru416@gmail.com>
2025-08-26 10:43:57 +0600

    26-4 Login Functionality & Redirect to Home

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // Login Button Functionality
 document.getElementById("loginButton").addEventListener("click", function (e) {
     e.preventDefault()
-    console.log("Login button clicked")
-    console.log(e)
+    const mobileNumber = 12345678910;
+    const pinNumber = 1234;
+    const mobileNumberValue = document.getElementById("mobile-number").value
+    const mobileNumberValueConverted = parseInt(mobileNumberValue);
+
+    const pinNumberValue = document.getElementById("pin-number").value
+    const pinNumberValueConverted = parseInt(pinNumberValue);
+
+    console.log(mobileNumberValueConverted, pinNumberValueConverted);
+
+    if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber){
+        window.location.href = "./home.html"
+    }
+    else{
+        alert("Invalid Credentials")
+    }
 })
